019c7a32ae2ea0a5d637a6ab7a370438562f8af7
Caleb Froese <caleb.froese@gmail.com>
2017-03-25 12:18:53 +1030

    Add user icon to the post table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('image') imageTpl: TemplateRef<any>;
   rows = [];
   loadingIndicator: boolean = true;
-
-  columns = [{prop: 'name'}];
+  columns: any[] = [];
 
   constructor() {
     this.fetch((data) => {
@@ -20,7 +20,33 @@ export class AppComponent {
     });
   }
 
+  ngOnInit() {
+    this.columns = [
+      {prop: 'image', cellTemplate: this.imageTpl}, {prop: 'name'},
+      {name: 'POST', prop: 'post.title'}
+    ];
+  }
+
   fetch(cb) {
-    cb([{name: 'John'}, {name: 'Bob'}, {name: 'Thomas'}]);
+    cb([
+      {
+        image:
+            'https://cdn.pixabay.com/photo/2016/03/31/19/10/avatar-1294774_960_720.png',
+        name: 'John',
+        post: {title: 'A test post'}
+      },
+      {
+        image:
+            'https://cdn.pixabay.com/photo/2016/03/31/19/57/avatar-1295406_960_720.png',
+        name: 'Bob',
+        post: {title: 'Something interesting'}
+      },
+      {
+        image:
+            'https://upload.wikimedia.org/wikipedia/commons/0/07/Avatar_girl_face.png',
+        name: 'Susan',
+        post: {title: 'Hey guys, come check out this forum!'}
+      }
+    ]);
   }
 }
